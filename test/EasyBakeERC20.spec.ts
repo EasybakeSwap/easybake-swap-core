b1f43b0329286675364de43d60887805a0e6e6eb
Uni <CryptoUnico@gmail.com>
2021-03-31 00:56:16 -0500

    one smol ting

--- test/EasyBakeERC20.spec.ts
+++ test/EasyBakeERC20.spec.ts
@@ -29,7 +29,7 @@ describe('EasyBakeERC20', () => {
 
   it('name, symbol, decimals, totalSupply, balanceOf, DOMAIN_SEPARATOR, PERMIT_TYPEHASH', async () => {
     const name = await token.name()
-    expect(name).to.eq('EasyBake LPs')
+    expect(name).to.eq('EasyBake LP')
     expect(await token.symbol()).to.eq('Oven-LP')
     expect(await token.decimals()).to.eq(18)
     expect(await token.totalSupply()).to.eq(TOTAL_SUPPLY)
